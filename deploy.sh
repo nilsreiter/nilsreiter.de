383fc6d95bfa45e28e4726b68d489638e6a40faf
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-19 06:01:12 +0100

    GH action

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
 
+TMP=TEMP
 
-case "$TRAVIS_BRANCH" in
- draft) TGT=beta ;;
- *) TGT=$DEPLOY_DIRECTORY ;;
-esac
+mkdir $TMP
+echo "$SSH_PRIVATE_KEY" > $TMP/deploy_rsa
+chmod 600 $TMP/deploy_rsa
+cp deploy_rsa.pub $TMP/
 
-rsync --compress --recursive --checksum --delete --quiet --delete-after _site/* $DEPLOY_USER@$DEPLOY_HOST:$TGT
+echo -e "Host $DEPLOY_HOST\n\tStrictHostKeyChecking no\n" > $TMP/config
+echo -e "IdentityFile $TMP/deploy_rsa\n" >> $TMP/config
+
+eval `ssh-agent -s`
+ssh-add $TMP/deploy_rsa
+
+TGT=$DEPLOY_DIRECTORY
+
+rsync -e "ssh -F $TMP/config -i $TMP/deploy_rsa" --compress --recursive --checksum --delete --quiet --delete-after _site/* $DEPLOY_USER@$DEPLOY_HOST:$TGT
+
+rm -rf $TMP
